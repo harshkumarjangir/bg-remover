fc2a77a3bad7bb115a9dbab6fd1ed1db054ace2d
Harsh Kumar Jangir <harshkumarjangir2002@gmail.com>
2025-02-27 00:37:56 +0530

    server code
    backend folder structure setup, server setup using express congig database using mongoose, dotenv, cors

---
+++ server/controllers/UserController.js
@@ -0,0 +1,6 @@
+// API Controller Function to Manage Clerk User with database.
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebHooks = async (req, res) => {
+
+}
---
+++ server/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+// App Config
+const PORT = process.env.PORT || 4000
+
+// Initializing the  Instance
+const app = express()
+await connectDB()
+
+// Initialize Middleware
+app.use(express.json()) // Whenever we get any request on the server then it will be parsed using the json() method.
+app.use(cors()) // By using cors we can connect our client running on that is running on the different port to the backend server.
+
+// API Routes
+app.get('/', (req, res) => res.send('API Working'))
+
+// Listening App
+app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
