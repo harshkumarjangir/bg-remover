f10c5efe55eec9b46be39156f9de47ee682c6c18
Harsh Kumar Jangir <harshkumarjangir2002@gmail.com>
2025-06-25 12:06:13 +0530

    controller update

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -6,8 +6,54 @@ import userModel from '../models/userModel.js'
 
 const aman = async (req, res) => {
     try {
-        res.send("Aman Webhook is working fine")
-        return
+
+
+        // Create Svix instance with clerk webhook secret.
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.body["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+
+        const { data, type } = req.body
+
+        switch (type) {
+            case "user.created": {
+                const userData = {
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url,
+                }
+                await userModel.create(userData)
+                res.json({})
+                break;
+            }
+            case "user.updated": {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url,
+                }
+
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
+                res.json({})
+                break;
+            }
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id })
+                res.json({})
+                break;
+            }
+
+            default:
+                break;
+        }
+
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message })
@@ -17,8 +63,7 @@ const aman = async (req, res) => {
 const clerkWebHooks = async (req, res) => {
     try {
 
-        res.send("Clerk Webhook is working fine")
-        return
+
         // Create Svix instance with clerk webhook secret.
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
